bca60f458144ba6b6367bb6266fb1ee0cce4db33
ConchiSca <71380049+ConchiSca@users.noreply.github.com>
2020-11-17 15:52:29 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+
+let colors = ['yellow', 'blue', 'red', 'orange'];
+
+// let i = 0;
+
+// while (i < colors.length) {
+//     console.log(colors[i]);
+//     i++;
+// }
+
+
+// for (i = 0; i < colors.length; i++){
+//     console.log(colors[i]);
+// }
+
+
+colors.forEach(element => console.log(element));
+
+// Hoeveel regels neemt mijn for loop en mijn while loop in beslag? 3 of 4
+//Hoeveel regels neemt mijn forEach method in beslag? 1
+//Ik vind de array method makkelijker omdat ik in dit geval geen 
+//extra values of index hoeft te zetten maar ik kan me voorstellen dat met 
+//grotere/meer complex arrays zijn while en for loops handiger?
